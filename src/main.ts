7979207faafe43400befcf3e48737569475c3d91
Septian Maulana <septiandev028@gmail.com>
2024-01-07 22:37:02 +0700

    feat: create add marker function
    refactor: refactor constructor in custom gogle map glas

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,17 @@
 import { CustomGoogleMap } from "./CustomGoogleMap";
 
-new CustomGoogleMap("map");
+const customGoogleMap = new CustomGoogleMap("map");
+
+customGoogleMap.addMarker({
+  location: {
+    lat: 0,
+    lng: 0,
+  },
+  markerContent: `
+      <div>
+        <h1>Company Name: Septian</h1>
+        <h3>Catchphrase: Hello</h3>
+      </div>
+    `,
+  color: "blue",
+});
